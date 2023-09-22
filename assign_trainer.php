41e36c835deaf45bdbfcff1ee13cab0a5469fc9e
Ahmed Jasarevic <66922656+ahmedjasarevic@users.noreply.github.com>
2023-09-22 15:10:32 +0200

    gym update

--- assign_trainer.php
+++ assign_trainer.php
@@ -5,5 +5,8 @@
 require_once 'config.php';
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-    
+    $member_id = $_POST['member'];
+    $trainer_id = $_POST['trainer'];
+
+    $sql = "UPDATE members SET trainer_id = ? WHERE member_id = ?"
 }
